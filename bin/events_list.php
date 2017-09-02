3513e6a91529835c0513b33d316e47b73e6c535b
Daisuke Inoue <hibariyashiki@gmail.com>
2017-09-02 20:23:54 +0900

    indent

--- bin/events_list.php
+++ bin/events_list.php
@@ -1,18 +1,18 @@
 <?php
 
-	if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-		require __DIR__ . '/../vendor/autoload.php';
-	} else {
-		require __DIR__ . '/../../../autoload.php';
-	}
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+	require __DIR__ . '/../vendor/autoload.php';
+} else {
+	require __DIR__ . '/../../../autoload.php';
+}
 	
-	use Sinsituwoka\Sinsituwoka;
-	use Mikatotai\Mikatotai;
-	use GuzzleHttp\Client;
+use Sinsituwoka\Sinsituwoka;
+use Mikatotai\Mikatotai;
+use GuzzleHttp\Client;
 
-	$m = new Mikatotai();
+$m = new Mikatotai();
 
-	$json = fgets(STDIN);
+$json = fgets(STDIN);
 $array = json_decode($json, true);
 
 $method = $array['method'];
@@ -21,4 +21,4 @@ $url = $array['url'];
 $query = $array['query'];
 
 
-	echo $m->generic($method, $url, $query);
+echo $m->generic($method, $url, $query);
